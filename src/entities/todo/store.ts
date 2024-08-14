fffd87db33dabe76bc3250e62417d609419e4a7d
Woollywood <115595112+Woollywood@users.noreply.github.com>
2024-08-14 14:15:19 +0300

    refactor

--- src/entities/todo/store.ts
+++ src/entities/todo/store.ts
@@ -2,14 +2,12 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { type Database } from '@/services/supabase';
 import { supabase } from '@/services/supabaseClient';
 
-type ColumnBase = Database['public']['Tables']['columns']['Row'];
-type TodoBase = Database['public']['Tables']['todos']['Row'];
-export interface Column extends ColumnBase {
-	todos: TodoBase[];
-}
+export type Column = Database['public']['Tables']['columns']['Row'];
+export type Todo = Database['public']['Tables']['todos']['Row'];
+
 interface InitialState {
 	columns: Column[];
-	todos: TodoBase[];
+	todos: Todo[];
 }
 
 const initialState: InitialState = {
@@ -17,8 +15,24 @@ const initialState: InitialState = {
 	todos: [],
 };
 
+export const updateColumnFromColumnId = createAsyncThunk(
+	'@@todo/updateColumn',
+	async (column: Column, { rejectWithValue }) => {
+		const { data, error } = await supabase
+			.from('columns')
+			.update({ ...column })
+			.eq('id', column.id)
+			.select()
+			.single();
+		if (error) {
+			return rejectWithValue(error.message);
+		}
+		return data;
+	}
+);
+
 export const deleteColumnFromColumnId = createAsyncThunk(
-	'@@/todos/deleteColumn',
+	'@@todo/deleteColumn',
 	async (columnId: string, { rejectWithValue }) => {
 		const { data, error } = await supabase.from('columns').delete().eq('id', columnId).select().single();
 		if (error) {
@@ -29,7 +43,7 @@ export const deleteColumnFromColumnId = createAsyncThunk(
 );
 
 export const createColumnFromUserId = createAsyncThunk(
-	'@@todos/createColumn',
+	'@@todo/createColumn',
 	async (userId: string, { rejectWithValue }) => {
 		const { data, error } = await supabase
 			.from('columns')
@@ -46,11 +60,25 @@ export const createColumnFromUserId = createAsyncThunk(
 export const getColumnsFromUserId = createAsyncThunk(
 	'@@todo/getColumns',
 	async (usedId: string, { rejectWithValue }) => {
-		const { data, error } = await supabase.from('columns').select('*, todos ( * )').eq('user_id', usedId);
-		if (error) {
-			return rejectWithValue(error.message);
+		const { data: columnsData, error: columnsError } = await supabase
+			.from('columns')
+			.select('*')
+			.eq('user_id', usedId)
+			.order('id');
+		if (columnsError) {
+			return rejectWithValue(columnsError.message);
 		}
-		return data;
+
+		const { data: todosData, error: todosError } = await supabase
+			.from('todos')
+			.select('*')
+			.eq('user_id', usedId)
+			.order('id');
+		if (todosError) {
+			return rejectWithValue(todosError.message);
+		}
+
+		return { columns: columnsData, todos: todosData };
 	}
 );
 
@@ -72,10 +100,11 @@ export const slice = createSlice({
 				state.todos = payload;
 			})
 			.addCase(getColumnsFromUserId.fulfilled, (state, { payload }) => {
-				state.columns = payload;
+				state.columns = payload.columns;
+				state.todos = payload.todos;
 			})
 			.addCase(createColumnFromUserId.fulfilled, (state, { payload }) => {
-				state.columns.push({ ...payload, todos: [] });
+				state.columns.push(payload);
 			})
 			.addCase(deleteColumnFromColumnId.fulfilled, (state, { payload }) => ({
 				columns: state.columns.filter((column) => column.id !== payload.id),
